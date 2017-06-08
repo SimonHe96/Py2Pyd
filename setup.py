22bef2dc697b8e655eb266f038a19df9e493dcd9
Changlong.Zang <zclongpop123@gmail.com>
2017-06-08 16:21:13 +0800

    use new method to build pyd...

---
+++ setup.py
@@ -0,0 +1,61 @@
+#========================================
+#    author: Changlong.Zang
+#      mail: zclongpop123@163.com
+#      time: Thu Jun 08 12:30:17 2017
+#========================================
+import os, re, time, shutil
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+tm = time.localtime()
+PYD_POST_FIX = '{0:0>4d}{1:0>2d}{2:0>2d}{3:0>2d}{4:0>2d}'.format(tm.tm_year, tm.tm_mon, tm.tm_mday, tm.tm_hour, tm.tm_min)
+
+def func(path):
+    '''
+    '''
+    for pt, ds, fs in os.walk(path):
+
+        #-
+        os.chdir(pt)
+
+        #-
+        for f in fs:
+            if not re.search('\.py$',  f):
+                continue
+
+            if re.match('setup.py',    f):
+                continue
+
+            if re.match('__init__.py', f):
+                continue
+
+            if re.search('_rc.py',     f):
+                continue
+
+            #-
+            origin_name = os.path.splitext(f)[0]
+            build_name  = '{0}_{1}'.format(origin_name, PYD_POST_FIX)
+            build_file  = '{0}.py'.format(build_name)
+
+            shutil.copy(f, build_file)
+            #-
+            extensions = [Extension(build_name, [ build_file ], include_dirs=[])]
+            setup(name=build_name, ext_modules=cythonize(extensions))
+
+            #- clean up
+            for ext in ('c', 'py', 'pyc'):
+                delete_file = '{0}.{1}'.format(build_name, ext)
+                os.path.isfile(delete_file) and os.remove(delete_file)
+
+            os.path.isfile('{0}.pyc'.format(origin_name)) and os.remove('{0}.pyc'.format(origin_name))
+
+            #-
+            with open(f, 'w') as fw:
+                fw.write('from {0} import *'.format(build_name))
+
+
+
+if __name__ == '__main__':
+    path = os.getcwd()
+    func(path)
